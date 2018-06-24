ac7fa24a14c8e23b1d394ed45babe11b2e7bba11
Amin Golmahalle <GolmahalehA@gaj.ir>
2018-06-24 14:44:05 +0430

    Add project files.

---
+++ CalcTraz/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace CalcTraz
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridViewX1 = new System.Windows.Forms.DataGridView();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblMax = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblMin = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewX1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.button1.Location = new System.Drawing.Point(166, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 36);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "محاسبه";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridViewX1
+            // 
+            this.dataGridViewX1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewX1.Location = new System.Drawing.Point(3, 5);
+            this.dataGridViewX1.Name = "dataGridViewX1";
+            this.dataGridViewX1.Size = new System.Drawing.Size(159, 457);
+            this.dataGridViewX1.TabIndex = 2;
+            // 
+            // listBox2
+            // 
+            this.listBox2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(421, 5);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(184, 459);
+            this.listBox2.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(375, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(178, 403);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Maximum";
+            // 
+            // lblMax
+            // 
+            this.lblMax.AutoSize = true;
+            this.lblMax.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMax.ForeColor = System.Drawing.Color.Purple;
+            this.lblMax.Location = new System.Drawing.Point(233, 403);
+            this.lblMax.Name = "lblMax";
+            this.lblMax.Size = new System.Drawing.Size(14, 15);
+            this.lblMax.TabIndex = 7;
+            this.lblMax.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.Color.Black;
+            this.label5.Location = new System.Drawing.Point(178, 441);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Minimum";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(327, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "تعداد";
+            // 
+            // lblMin
+            // 
+            this.lblMin.AutoSize = true;
+            this.lblMin.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMin.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.lblMin.Location = new System.Drawing.Point(233, 441);
+            this.lblMin.Name = "lblMin";
+            this.lblMin.Size = new System.Drawing.Size(14, 15);
+            this.lblMin.TabIndex = 11;
+            this.lblMin.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(615, 470);
+            this.Controls.Add(this.lblMin);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblMax);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.dataGridViewX1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Taraz";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewX1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridViewX1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblMax;
+        private System.Windows.Forms.Label label5;       
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblMin;
+    }
+}
+
---
+++ CalcTraz/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Globalization;
+using System.Windows.Forms;
+
+namespace CalcTraz
+{
+    public partial class Form1 : Form
+    {
+        #region Fields
+        private readonly SqlConnection _conn = new SqlConnection();
+        private int _count;
+        private DataTable _dt;
+        #endregion
+
+        #region Constructore
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        #endregion
+
+        #region Functions
+        public double CalcStdev(double avg, double val)
+        {            
+            double s = 0;
+            for (int i = 0; i < _count; i++)
+            {
+                s += Math.Pow(Convert.ToDouble(dataGridViewX1.Rows[i].Cells[0].Value.ToString()) - avg, 2);
+            }
+            s = s / _count;//واریانس
+            s = Math.Pow(s, val);//انحراف معیار
+            return s;
+        }
+        #endregion
+
+        #region Events
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(dataGridViewX1.Rows.Count==0)
+                return;
+
+            _count = dataGridViewX1.Rows.Count - 1;
+
+            List<double> list2 = new List<double>();
+            double sum = 0;
+            for (int i = 0; i < _count; i++)
+            {
+                sum += Convert.ToDouble(dataGridViewX1.Rows[i].Cells[0].Value.ToString());
+            }
+
+            sum = sum / _count;//average
+
+            double sqrtIndex = .50;
+            double zaribEtminan = 1;
+            var stdev = CalcStdev(sum, sqrtIndex);
+
+            for (int i = 0; i < _count; i++)
+            {
+                var z =( Convert.ToDouble(dataGridViewX1.Rows[i].Cells[0].Value.ToString()) - sum)/ stdev;
+                var resultTraz = Math.Floor( Convert.ToInt32(1000 * z+ 5000)* zaribEtminan);
+
+                if (resultTraz > 10000)
+                {
+                    zaribEtminan = .99;
+                    sqrtIndex = sqrtIndex + .01;
+                    stdev = CalcStdev(sum, sqrtIndex);
+                    i = -1;
+                    list2.Clear();
+                }
+
+                else
+                {
+                    list2.Add(resultTraz);
+                }
+            }
+
+            listBox2.DataSource = list2;
+            label2.Text = list2.Count.ToString();
+
+            //max min value ListBox2
+            double max = Convert.ToDouble(listBox2.Items[0]);
+            double min = max;
+
+            foreach (var item in listBox2.Items)
+            {
+                var cng = Convert.ToDouble(item);
+                if (cng > max)
+                    max = cng;
+                else if (cng < min)
+                    min = cng;
+            }
+           
+            lblMax.Text = max.ToString(CultureInfo.InvariantCulture);
+            lblMin.Text = min.ToString(CultureInfo.InvariantCulture);
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+             _dt = new DataTable();
+            _conn.ConnectionString = System.Configuration.ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+            _conn.Open();
+            string query = "Select F_nmrh From NmrhFard WHERE f_drs=3 AND F_ID=139623322";
+            SqlDataAdapter da = new SqlDataAdapter(query, _conn);
+            da.Fill(_dt);
+            dataGridViewX1.DataSource = _dt;
+        }
+        #endregion
+    }
+}
